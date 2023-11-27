ee8f350dec6d7fa25e7ea88e09330cf94d612ac5
lumijoe <spacedesign63@gmail.com>
2023-11-27 13:38:35 +0900

    fix App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+// list2-2-3(p60)1127
 import logo from './logo.svg';
 import './App.css';
 
+// Appコンポーネント
 function App() {
+  // 描画生成
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        <a 
+          className='App-link'
+          href='https://reactjs.org'
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Learn React
         </a>
       </header>
     </div>
   );
 }
 
+// App関数のエクスポート
 export default App;
